1d727a1e089fcfdd68f23a87d0137e51e49a495e
NicholasGomis <nicholasgomis@icloud.com>
2022-06-06 16:42:54 +0100

    randomized the pokemon

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+
+let randomPokemon = Math.floor((Math.random() * 10) + 1);
+
 async function getPokemon(){
-    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/2`);
+    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
     const data = await response.json();
     console.log(data) // Shows all objects containing in pokemon number 2 payload
     console.log(data.forms[0].name) // get the name of the pokemon
