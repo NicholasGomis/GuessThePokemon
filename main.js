4cc4a62050afe0613f9439e43b3b4d14d0efe270
NicholasGomis <nicholasgomis@icloud.com>
2022-06-06 16:40:22 +0100

    manipulating the api to get interested data

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 async function getPokemon(){
     const response = await fetch (`https://pokeapi.co/api/v2/pokemon/2`);
     const data = await response.json();
-    console.log(data)
+    console.log(data) // Shows all objects containing in pokemon number 2 payload
+    console.log(data.forms[0].name) // get the name of the pokemon
+    console.log(data.sprites.back_default)  // Get the image of the pokemon
+   
 }
 
 getPokemon()
