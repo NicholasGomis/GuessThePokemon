90766b48c79fffa6df8c8294abc1301d56952834
NicholasGomis <nicholasgomis@icloud.com>
2022-06-07 16:37:20 +0100

    Spotted problem, updated plan and randomised number

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
+let pokemonsAnswers = [];
+let randomPokemon = Math.floor((Math.random() * 100) + 1);
+let randomPokemonTwo = Math.floor((Math.random() * 100) + 1);
+let randomPokemonThree = Math.floor((Math.random() * 100) + 1);
 
-let randomPokemon = Math.floor((Math.random() * 10) + 1);
-let randomPokemonTwo = Math.floor((Math.random() * 10) + 1);
-let randomPokemonThree = Math.floor((Math.random() * 10) + 1);
 
-async function getPokemon(){
-    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
-    const data = await response.json();
-    console.log(data) // Shows all objects containing in pokemon number 2 payload
-    console.log(data.forms[0].name) // get the name of the pokemon
-    console.log(data.sprites.back_default)  // Get the image of the pokemon
-   
+let generateNumbers = (number) => {
+
+pokemonsAnswers.push(randomPokemon);
+
+if( randomPokemonTwo!= pokemonsAnswers && randomPokemonThree != pokemonsAnswers){
+
+    pokemonsAnswers.push(randomPokemonTwo);
+    pokemonsAnswers.push(randomPokemonThree);
+} else {
+    
+    console.log("doubled")
+}
+return pokemonsAnswers
+
 }
 
-async function getPokemonTwo(){
-    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonTwo}`);
-    const data = await response.json();
-    console.log(data) // Shows all objects containing in pokemon number 2 payload
-    console.log(data.forms[0].name) // get the name of the pokemon
-    console.log(data.sprites.back_default)  // Get the image of the pokemon
-   
-}
 
-async function getPokemonThree(){
-    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonThree}`);
-    const data = await response.json();
-    console.log(data) // Shows all objects containing in pokemon number 2 payload
-    console.log(data.forms[0].name) // get the name of the pokemon
-    console.log(data.sprites.back_default)  // Get the image of the pokemon
-   
-}
-getPokemon()
-getPokemonTwo()
-getPokemonThree()
+
+// pokemonsAnswers.push(randomPokemon);
+
+// if( randomPokemonTwo!= pokemonsAnswers && randomPokemonThree != pokemonsAnswers){
+
+//     pokemonsAnswers.push(randomPokemonTwo);
+//     pokemonsAnswers.push(randomPokemonThree);
+// } else {
+    
+//     console.log("doubled")
+// }
+
+
+
+
+// async function fetchPokemons(){
+//     const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
+//     const data = await response.json();
+//     console.log(data) // Shows all objects containing in pokemon number 2 payload
+//     console.log(data.forms[0].name) // get the name of the pokemon
+//     console.log(data.sprites.back_default)  // Get the image of the pokemon
+
+//    const responseTwo = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonTwo}`);
+//    const dataTwo = await responseTwo.json();
+//    console.log(dataTwo) // Shows all objects containing in pokemon number 2 payload
+//    console.log(dataTwo.forms[0].name) // get the name of the pokemon
+
+//     const responseThree = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonThree}`);
+//     const dataThree = await responseThree.json();
+//     console.log(dataThree) // Shows all objects containing in pokemon number 2 payload
+//     console.log(dataThree.forms[0].name) // get the name of the pokemon
+// }
+
+// fetchPokemons()
+console.log(generateNumbers(pokemonsAnswers))
--- planning/planning.md
+++ planning/planning.md
@@ -8,4 +8,11 @@
 ## Step 2
 
 * Randomize the payload of the pokemon rather than be hardcoded - DONE.
-* Try to display three different pokemonz at time in console (Name and image)
+* Need to display one image of a pokemon (console).
+* Try to display two different pokemon names (two randoms and the other that matches with the linked image).
+* Need to check IF the fetch image is equal to the ID of the pokemon.
+* IF YES EQUAL = Right answer +1
+* IF NOT EQUAL = Wrong answer 0
+
+
+* Problem: The method above will have some probabily that the number generated might be the same, causing problem/
