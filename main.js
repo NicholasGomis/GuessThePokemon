7daf70bd21e88ffd91da11fd76482a3d88b90eb7
NicholasGomis <nicholasgomis@icloud.com>
2022-06-07 15:10:10 +0100

    Fetched two extra pokemon names

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
 let randomPokemon = Math.floor((Math.random() * 10) + 1);
+let randomPokemonTwo = Math.floor((Math.random() * 10) + 1);
+let randomPokemonThree = Math.floor((Math.random() * 10) + 1);
 
 async function getPokemon(){
     const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
@@ -10,4 +12,23 @@ async function getPokemon(){
    
 }
 
+async function getPokemonTwo(){
+    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonTwo}`);
+    const data = await response.json();
+    console.log(data) // Shows all objects containing in pokemon number 2 payload
+    console.log(data.forms[0].name) // get the name of the pokemon
+    console.log(data.sprites.back_default)  // Get the image of the pokemon
+   
+}
+
+async function getPokemonThree(){
+    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${randomPokemonThree}`);
+    const data = await response.json();
+    console.log(data) // Shows all objects containing in pokemon number 2 payload
+    console.log(data.forms[0].name) // get the name of the pokemon
+    console.log(data.sprites.back_default)  // Get the image of the pokemon
+   
+}
 getPokemon()
+getPokemonTwo()
+getPokemonThree()
--- planning/planning.md
+++ planning/planning.md
@@ -1,6 +1,11 @@
 ## Step 1
 
-* Use the Pokemon API
-* Start manipulate the api and understand the data structure.
-* Refresh how to fetch api from the main JS.
-* Shows any data from the api data
+* Use the Pokemon API - DONE
+* Start manipulate the api and understand the data structure. - DONE
+* Refresh how to fetch api from the main JS. - DONE
+* Shows any data from the api data - DONE
+
+## Step 2
+
+* Randomize the payload of the pokemon rather than be hardcoded - DONE.
+* Try to display three different pokemonz at time in console (Name and image)
